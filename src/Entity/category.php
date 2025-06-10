3bcc40621ccae05f7c061de3983bc51d8abfc7bb
Yannyck Djo-djolo Ayessa <yannyck.djo-djolo-ayessa@etudiant.univ-reims.fr>
2025-06-10 10:42:21 +0200

    creation of the class category

---
+++ src/Entity/category.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity;
+
+class category
+{
+    private int $id;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+
+
+}
