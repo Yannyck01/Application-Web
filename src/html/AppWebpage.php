fd033f6fa1fc19b505ec5645937bd14c19a16313
Yannyck Djo-djolo Ayessa <yannyck.djo-djolo-ayessa@etudiant.univ-reims.fr>
2025-06-10 09:50:44 +0200

    Add of the function toHTML for the AppWebPage class

--- src/html/AppWebpage.php
+++ src/html/AppWebpage.php
@@ -8,4 +8,40 @@ class AppWebPage extends WebPage{
     {
         parent::__construct($title);
     }
+    public function toHTML(): string
+    {
+
+        $HTML = <<<HTML
+
+        <!DOCTYPE html>
+        <html lang="fr">
+        <head>
+        <meta charset="UTF-8" name="viewport">
+                <title>{$this->getTitle()}</title>
+        
+        </head>
+        <body>
+            <header class="header">
+                <h1>{$this->getTitle()}</h1>
+            </header>
+            <br>
+         <div class="content">
+
+            {$this->getBody()}
+
+        </div>
+
+         
+
+          <footer class="footer">
+          {$this->getLastModification()}
+          </footer>
+         </body>
+        </html>
+
+
+        HTML;
+        return $HTML;
+
+    }
 }
