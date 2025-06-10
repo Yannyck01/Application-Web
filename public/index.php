9b27d818b357852e2a98b8337e047e6812ba3281
Raphaël WAYMEL <raphael.waymel-beviere@etudiant.univ-reims.fr>
2025-06-10 14:00:38 +0200

    feat: add style.css to improve index.php

--- public/index.php
+++ public/index.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 use Html\WebPage;
 
 $pageTest = new WebPage("Page d'accueil");
+$pageTest->appendCssUrl("css/style.css");
 
 $pageTest->appendContent("<h1>Jeux vidéo</h1>");
 
 $pageTest->appendContent("<div class='list'>");
+$pageTest->appendContent("<h1>Genres</h1>");
 
 $genres = \Entity\Collection\GenreCollection::findAll();
 $categories = \Entity\Collection\CategoryCollection::findAll();
@@ -16,7 +18,9 @@ $categories = \Entity\Collection\CategoryCollection::findAll();
 foreach ($genres as $genre) {
     $pageTest ->appendContent("<p><a href='genre?Id = {$genre->getId()}'><br>{$pageTest->escapeString($genre->getDescription())}</a></p><br>");
 }
+$pageTest->appendContent("<h1>Catégories</h1>");
 foreach ($categories as $category) {
-    $pageTest->appendContent("<p><a href='category?Id = {$category->getId()}'><br>");
+    $pageTest->appendContent("<p><a href='category?Id = {$category->getId()}'><br>{$pageTest->escapeString($category->getDescription())}</a></p><br>");
 }
+
 echo $pageTest->toHTML();
