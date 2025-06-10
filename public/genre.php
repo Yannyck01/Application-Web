7666083b0bdfa736a97c8db977468b96b7cbb440
Yannyck Djo-djolo Ayessa <yannyck.djo-djolo-ayessa@etudiant.univ-reims.fr>
2025-06-10 14:21:36 +0200

    Utilisation de la liste des jeux d'un genre

--- public/genre.php
+++ public/genre.php
@@ -1,2 +1,16 @@
 <?php
 declare(strict_types=1);
+
+use Database\MyPdo;
+use Entity\Game;
+use html\WebPage;
+
+$idVoulu = "Unknown";
+if (isset($_GET['idGenre'])&& ctype_digit($_GET['idGenre'])) {
+    $idGenre = (int) $_GET['idGenre'];
+}
+
+$genreDesc = new \Html\WebPage("genre description");
+
+$genreRequest =  Game::findByGenreId($idGenre);
+
