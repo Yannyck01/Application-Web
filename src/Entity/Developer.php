3b797e518f13da340350e762537f94ce7f4228aa
Raphaël WAYMEL <raphael.waymel-beviere@etudiant.univ-reims.fr>
2025-06-12 10:27:10 +0200

    feat: add findById() in Developer.php

---
+++ src/Entity/Developer.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+
+class Developer
+{
+    private int $id;
+    private string $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public static function findById(int $id): Developer
+    {
+        $sql=MyPdo::getInstance()->prepare("SELECT * FROM developer WHERE id = :id");
+
+        $sql->execute(["id" => $id]);
+
+        $res = $sql->fetchObject(Developer::class);
+
+        if (!$res)
+            throw new EntityNotFoundException("Developer with Id $id not found");
+
+        return $res;
+    }
+
+}
