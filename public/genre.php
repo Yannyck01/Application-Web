3d7a235d005ccc17897a5510e68b8fdf89ffc1f7
Yannyck Djo-djolo Ayessa <yannyck.djo-djolo-ayessa@etudiant.univ-reims.fr>
2025-06-12 10:06:21 +0200

    dev: Add of the home page button in the list of game for a genre

--- public/genre.php
+++ public/genre.php
@@ -25,7 +25,17 @@ $genreObject=Genre::findById((int) $genreId);
 $webPage = new WebPage();
 $webPage->setTitle("Jeux vidéo : {$webPage->escapeString($genreObject->getDescription())}");
 $webPage->appendCssUrl("css/style.css");
-$webPage->appendContent("<div class='header'> <h1>Jeux vidéo : {$webPage->escapeString($genreObject->getDescription())}</h1></div>");
+$webPage->appendContent(<<<HTML
+    <div class='header'> 
+        <h1>Jeux vidéo : {$webPage->escapeString($genreObject->getDescription())}</h1>
+        <button class ='homepage' type='button'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+          <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
+          <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
+        </svg>
+        </button>
+    </div>
+HTML);
 
 $webPage->appendContent('<div class="list">');
 foreach ($games as $game) {
