43c56fb243847eae5b79daf7ebfaca4bf2a63bd8
Yannyck Djo-djolo Ayessa <yannyck.djo-djolo-ayessa@etudiant.univ-reims.fr>
2025-06-13 14:12:00 +0200

    Dev: Add of the function getHtmlForm to create the matching form which will allows users to create or add a game

--- src/Entity/form/GameForm.php
+++ src/Entity/form/GameForm.php
@@ -28,6 +28,35 @@ class GameForm
         return $this->game;
     }
 
+    public function getHtmlForm(string $action): string
+    {
+        return <<<HTML
+        <form name="gameForm"  action="$action" method="POST">
+            <div class="container">
+                <h1>Création d'un nouveau jeu</h1>
+                <label>
+                    <input name="game__name" type="text" placeholder="Game Name" value="{$this->game->getName()}">
+                </label>
+                <label>
+                    <input name="id" type="hidden" placeholder="ID" value="{$this->game?->getId()}">
+                </label>
+                <label>
+                    <input name="desc" type="text" placeholder="Description" value="{$this->game->getShortDescription()}">
+                </label>
+                <label>
+                    <input name="price__euro" type="number" placeholder="price in euro" value="{$this->game->getPrice()}">
+                </label>
+                <label>
+                    <input name="grade__100" type="number" placeholder="Grade out of 100" value="{$this->game->getMetacritic()}">
+                </label>
+            </div>
+        </form>
+
+    HTML;
+
+    }
+
+
 
 
 
